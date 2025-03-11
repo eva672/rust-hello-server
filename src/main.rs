cedd068b6db7a1f4416d4e11bedd51917d203663
eva672 <evamanuska@gmail.com>
2025-03-11 07:50:09 +0100

    updated cargo.toml and main.rs

--- src/main.rs
+++ src/main.rs
@@ -14,14 +14,20 @@
 //    
 //    .unwrap();
 
-use std::net::TcpListener;
+use axum::{Router, routing::get};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+async fn hello_world() -> &'static str {
+    "Hello world!"
+}
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
+#[tokio::main] 
+async fn main() {
+    let router = Router::new().route("/", get(hello_world));
 
-        println!("Connection established!");
-    }
+    let addr = SocketAddr::from(([127,0,0,1], 8000));
+    let tcp = TcpListener::bind(&addr).await.unwrap();
+
+    axum::serve(tcp, router).await.unwrap();
 }
