8b4c9155453af7844000da2c8c11554c99b62f01
eva672 <evamanuska@gmail.com>
2025-03-12 09:32:39 +0100

    updated cargo.toml and main.rs created a folder and a file into the folder

--- src/main.rs
+++ src/main.rs
@@ -11,23 +11,52 @@
 //    println!("Listening on {addr}\n");
 //    axum::Server::bind(&addr)
 //    .serve(routes_hello.into_make_device();)
-//    
+//
 //    .unwrap();
 
-use axum::{Router, routing::get};
-use std::net::SocketAddr;
-use tokio::net::TcpListener;
+use axum::{extract::Multipart, response::Html, routing::get, Router};
+use std::{fs::File, io::Write};
 
-async fn hello_world() -> &'static str {
-    "Hello world!"
+async fn index() -> Html<&'static str> {
+    Html(std::include_str!("../public/index.html"))
 }
 
-#[tokio::main] 
+async fn upload(mut multipart: Multipart) {
+    while let Some(field) = multipart
+        .next_field()
+        .await
+        .expect("Failed to get next field!")
+    {
+        if field.name().unwrap() != "fileupload" {
+            continue;
+        }
+
+        // Grab the name
+        let file_name = field.file_name().unwrap();
+
+        // Create a path for the soon-to-be file
+        let file_path = format!("files/{}", file_name);
+
+        // Unwrap the incoming bytes
+        let data = field.bytes().await.unwrap();
+
+        // Open a handle to the file
+        let mut file_handle = File::create(file_path).expect("Failed to open file handle!");
+
+        // Write the incoming data to the handle
+        file_handle.write_all(&data).expect("Failed to write data!");
+    }
+}
+
+#[tokio::main]
 async fn main() {
-    let router = Router::new().route("/", get(hello_world));
+    let app = Router::new().route("/", get(index).post(upload));
 
-    let addr = SocketAddr::from(([127,0,0,1], 8000));
-    let tcp = TcpListener::bind(&addr).await.unwrap();
+    let listener = tokio::net::TcpListener::bind("localhost:3000")
+        .await
+        .expect("Failed to start listener!");
 
-    axum::serve(tcp, router).await.unwrap();
+    axum::serve(listener, app)
+        .await
+        .expect("Failed to serve 'app'!");
 }
